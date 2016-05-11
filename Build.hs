b85624075c17dd869b9c3cac9a86ea9011460e63
Justus Adam <dev@justus.science>
2016-05-11 17:39:04 +0200

    Executing latex build twice

--- Build.hs
+++ Build.hs
@@ -5,15 +5,15 @@ import Development.Shake.Util
 
 buildDir = "_build"
 sourceDir = "src"
- 
+
 main :: IO ()
 main = shakeArgs shakeOptions{shakeFiles=buildDir} $ do
     want ["thesis.pdf"]
-    
+
     "*.pdf" %> \out -> do
         let srcRel =  out -<.> "tex"
         let src = sourceDir </> srcRel
         need [src]
-        Exit e <- command [Cwd sourceDir] "pdflatex" ["-interaction=nonstopmode", srcRel]
-        -- trackWrite [buildDir </> out] 
+        [_, Exit e] <- replicateM 2 $ command [Cwd sourceDir] "pdflatex" ["-interaction=nonstopmode", srcRel]
+        -- trackWrite [buildDir </> out]
         copyFileChanged (sourceDir </> out) out
