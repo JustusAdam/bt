c74c3344eb8d1abc31e6440df197ecac565b5519
Justus Adam <dev@justus.science>
2016-05-12 16:44:03 +0200

    fixes build

--- Build.hs
+++ Build.hs
@@ -2,18 +2,25 @@ import Development.Shake
 import Development.Shake.Command
 import Development.Shake.FilePath
 import Development.Shake.Util
+import Control.Monad (replicateM)
 
 buildDir = "_build"
 sourceDir = "src"
+
+
+buildPFD :: FilePath -> Action ()
+buildPFD out = do
+    need [src]
+    [_, Exit e] <- replicateM 2 $ command [Cwd sourceDir] "pdflatex" ["-interaction=nonstopmode", srcRel]
+    -- trackWrite [buildDir </> out]
+    copyFileChanged (sourceDir </> out) out
+  where
+      srcRel =  out -<.> "tex"
+      src = sourceDir </> srcRel
+
 
 main :: IO ()
 main = shakeArgs shakeOptions{shakeFiles=buildDir} $ do
     want ["thesis.pdf"]
 
-    "*.pdf" %> \out -> do
-        let srcRel =  out -<.> "tex"
-        let src = sourceDir </> srcRel
-        need [src]
-        [_, Exit e] <- replicateM 2 $ command [Cwd sourceDir] "pdflatex" ["-interaction=nonstopmode", srcRel]
-        -- trackWrite [buildDir </> out]
-        copyFileChanged (sourceDir </> out) out
+    "*.pdf" %> buildPFD
