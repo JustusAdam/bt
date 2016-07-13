a0a99aad5fc93d0e970171dc39337adf9be6751a
Justus Adam <dev@justus.science>
2016-07-13 14:43:42 +0200

    Scripts and a first version of the presentation slides

---
+++ midway-presentation/build.sh
@@ -0,0 +1,22 @@
+BUILD_DIR=_build
+if [ ! -d $BUILD_DIR ];
+then
+    mkdir $BUILD_DIR;
+fi
+if [ ! -d $BUILD_DIR/images ];
+then
+    mkdir $BUILD_DIR/images;
+fi
+
+cp images/* $BUILD_DIR/images
+
+cp style.css $BUILD_DIR
+pandoc -s -t --toc revealjs \
+    --slide-level=2 \
+    -V slideNumber="\"c\"" \
+    -V controls=false \
+    -V theme=white \
+    presentation.md \
+    -o _build/index.html \
+    -V margin=0.08 --css style.css \
+    --template=template.html
---
+++ midway-presentation/deploy.sh
@@ -0,0 +1,1 @@
+rsync -r _build/* justusad@octans.uberspace.de:/var/www/virtual/justusad/static.justus.science/presentations/bachelor-thesis-introduction
